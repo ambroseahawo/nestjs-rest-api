decfa367c937bea444486789ef1a8345ffee03ce
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-01-29 11:20:03 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+require("module-alias/register");
+
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "@src/app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap().catch((err) => {
+  console.log("Error during app bootstrap: ", err);
+  process.exit(1);
+});
