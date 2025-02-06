a1bc749e7890d92f612fb34d2a4a4463387b98c3
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-02-06 18:02:27 +0300

    Setup postgres orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { RecipeModule } from "@modules/recipe/recipe.module";
 
 @Module({
-  imports: [RecipeModule],
+  imports: [
+    RecipeModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [],
+      synchronize: true, // don't include on production
+      logging: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
