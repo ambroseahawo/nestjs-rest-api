1d9be2d92d45fcdf615adf53b8d6c39869154c99
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-02-18 09:30:01 +0300

    Configure global api prefix

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { AppModule } from "@src/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("/api/v1");
   app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
