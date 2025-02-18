0b43bf43ccab800986d70ce011635de09eecd9ad
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-02-18 18:40:44 +0300

    Update typeorm cli config

--- src/config/typeorm-cli.config.ts
+++ src/config/typeorm-cli.config.ts
@@ -2,8 +2,10 @@ import { ConfigService } from "@nestjs/config";
 import { config } from "dotenv";
 import { DataSource } from "typeorm";
 
+import { User } from "@modules/auth/entity/user";
 import { Ingredient, Recipe } from "@modules/recipe/entity/recipe";
 import { InitialSchema1739253188790 } from "@src/migrations/1739253188790-initial-schema";
+import { AuthSchema1739885265251 } from "@src/migrations/1739885265251-auth-schema";
 
 config();
 
@@ -17,6 +19,6 @@ export default new DataSource({
   password: configService.get<string>("DB_PASSWORD"),
   database: configService.get<string>("DB_NAME"),
   logging: configService.get<boolean>("DB_LOGGING"),
-  entities: [Recipe, Ingredient],
-  migrations: [InitialSchema1739253188790],
+  entities: [Recipe, Ingredient, User],
+  migrations: [InitialSchema1739253188790, AuthSchema1739885265251],
 });
