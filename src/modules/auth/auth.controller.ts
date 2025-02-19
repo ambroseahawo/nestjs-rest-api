644cd800e846fc1efc92a83d32ad795b908c831f
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-02-19 08:15:58 +0300

    Implement access and refresh tokens

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from "@nestjs/common";
 
 import { AuthService } from "@modules/auth/auth.service";
-import { RegisterDto } from "@modules/auth/dto/auth.dto";
+import { LoginDto, RefreshTokenDto, RegisterDto } from "@modules/auth/dto/auth.dto";
 
 @Controller("auth")
 export class AuthController {
@@ -10,5 +10,15 @@ export class AuthController {
   @Post("/register")
   async register(@Body() registerDto: RegisterDto) {
     return await this.authService.createUser(registerDto);
+  }
+
+  @Post("/login")
+  async login(@Body() loginDto: LoginDto) {
+    return await this.authService.login(loginDto);
+  }
+
+  @Post("/refresh-token")
+  async refreshToken(@Body() { refreshToken }: RefreshTokenDto) {
+    return await this.authService.refreshTokens(refreshToken);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,15 +1,26 @@
 import { HttpException, Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtService } from "@nestjs/jwt";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
 
-import { RegisterDto } from "@modules/auth/dto/auth.dto";
+import { LoginDto, RegisterDto } from "@modules/auth/dto/auth.dto";
 import { User, UserRole } from "@modules/auth/entity/user";
+
+export interface JwtTokens {
+  accessToken: string;
+  refreshToken: string;
+}
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
 
   async createUser(registerDto: RegisterDto) {
     const existingUser = await this.userRepository.findOne({ where: { email: registerDto.email } });
@@ -26,7 +37,63 @@ export class AuthService {
     });
   }
 
+  async login(loginDto: LoginDto): Promise<JwtTokens> {
+    const { email, password } = loginDto;
+    const user = await this.userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new HttpException("Invalid credentials", 400);
+    }
+
+    const validPassword = await compare(password, user.password);
+    if (!validPassword) {
+      throw new HttpException("Invalid credentials", 400);
+    }
+
+    return this.getTokens(user);
+  }
+
+  async refreshTokens(token: string): Promise<JwtTokens> {
+    try {
+      const { sub: email } = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get<string>("JWT_REFRESH_TOKEN_SECRET"),
+      });
+      const user = await this.userRepository.findOneOrFail({ where: { email } });
+
+      return this.getTokens(user);
+    } catch (error) {
+      throw new HttpException("Invalid credentials", 400);
+    }
+  }
+
   private hashPassword(password: string): Promise<string> {
     return hash(password, 10);
+  }
+
+  private async getTokens(user: User): Promise<JwtTokens> {
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: user.email,
+          role: user.role,
+        },
+        {
+          secret: this.configService.get<string>("JWT_ACCESS_TOKEN_SECRET"),
+          expiresIn: this.configService.get<string>("JWT_ACCESS_TOKEN_EXPIRATION"),
+        },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: user.email,
+          role: user.role,
+        },
+        {
+          secret: this.configService.get<string>("JWT_REFRESH_TOKEN_SECRET"),
+          expiresIn: this.configService.get<string>("JWT_REFRESH_TOKEN_EXPIRATION"),
+        },
+      ),
+    ]);
+
+    return { accessToken, refreshToken };
   }
 }
